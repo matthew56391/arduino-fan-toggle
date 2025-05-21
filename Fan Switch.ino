212e766cdbce293f53c98f8a291b847278550d47
Matthew Cuebas <cuebas429@gmail.com>
2025-05-20 22:15:08 -0500

    Add files via upload

---
+++ Fan Switch.ino
@@ -0,0 +1,23 @@
+int fanPin = 8;
+int buttonPin = 2;
+
+bool fanOn = false;
+int lastButtonState = LOW;
+
+void setup() {
+  pinMode(fanPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH && lastButtonState == LOW) {
+    fanOn = !fanOn;
+    digitalWrite(fanPin, fanOn ? HIGH : LOW);
+    delay(300);
+  }
+
+  lastButtonState = buttonState;
+
+}
